9acc91a8fd82d16cb43e0ed0164bac487bdc6b60
Tyler Knapp <tgknapp11@gmail.com>
2019-08-16 11:35:57 -0400

    Milestone index only displays User's milestones

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,6 @@ class SessionsController < ApplicationController
     def new
     end
 
-    # def create
-    #     @user = User.find_by(:username => params[:username])
-    #     session[:user_id] = @user[:id]
-    #     redirect_to root_path
-    # end
-
     def create
         @user = User.find_by(:username => params[:username])
         if @user
